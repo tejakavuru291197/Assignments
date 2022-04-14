f59241dd1c4fb79336749097db35ff4ffc2f0782
tejakavuru291197 <102531123+tejakavuru291197@users.noreply.github.com>
2022-04-14 12:48:44 +0530

    Add files via upload

---
+++ C#Assignment3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethod
+{
+    internal class Program
+    {
+        static void Method(int[] integer)
+        {
+            int sum = 0;
+            for (int i = 0; i < integer.Length; i++)
+                sum += integer[i];
+
+            Console.WriteLine("Sum of all integers {0}", sum);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Values");
+            int[] integer = new int[5];
+            for (int i = 0; i < integer.Length; i++)
+                integer[i] = Convert.ToInt32(Console.ReadLine());
+            Method(integer);
+        }
+    }
+}
---
+++ C#Assignment4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapofTwoIntegers
+{
+    internal class Program
+    {
+        static void Swap(int a, int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("A: {0}\nB: {1}", a, b);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Swap(a, b);
+        }
+    }
+}
